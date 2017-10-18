4fd1c5df1cfa6ef9573fc232ee19eee8dbba7ea1
Min Soo Kim <minskim92@gmail.com>
2017-10-18 13:22:43 -0400

    added public path for react-router

--- webpack.config.js
+++ webpack.config.js
@@ -6,75 +6,76 @@ const autoprefixer = require('autoprefixer');
 
 const config = {
   resolve: {
-    extensions: ['*', '.js', '.jsx']
+    extensions: ['*', '.js', '.jsx'],
   },
   devtool: 'source-map',
   entry: [
-    path.resolve(__dirname, 'src/index.jsx')
+    path.resolve(__dirname, 'src/index.jsx'),
   ],
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].bundle.js'
+    publicPath: '/',
+    filename: '[name].bundle.js',
   },
   module: {
     rules: [
-        { 
-          test: /\.jsx?$/, 
-          exclude: /node_modules/, 
-          loader: 'babel-loader'
-        },
-        {
-          test: /(\.css|\.scss|\.sass)$/,
-          // use ExtractTextPlugin so they can be outputted to their own .css file
-          use: ExtractTextPlugin.extract({
-            use: [
-              { // translates CSS into CommonJS
-                loader: 'css-loader',
-                options: { sourceMap: true }
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /(\.css|\.scss|\.sass)$/,
+        // use ExtractTextPlugin so they can be outputted to their own .css file
+        use: ExtractTextPlugin.extract({
+          use: [
+            { // translates CSS into CommonJS
+              loader: 'css-loader',
+              options: { sourceMap: true },
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                sourceMap: true,
+                plugins() { return [autoprefixer({ browsers: 'last 3 versions' })]; },
               },
-              {
-                loader: 'postcss-loader',
-                options: { 
-                  sourceMap: true,
-                  plugins() { return [autoprefixer({ browsers: 'last 3 versions' })]; }
-                }
-              },
-              { // compiles Sass to CSS
-                loader: 'sass-loader',
-                options: { sourceMap: true }
-              }
-            ],
-            // creates style nodes from JS strings
-            fallback: 'style-loader'
-          })
-        }, // Should work for Bootstrap
-        {test: /\.eot(\?v=\d+.\d+.\d+)?$/, loader: 'file-loader'},
-        {test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff'},
-        {test: /\.[ot]tf(\?v=\d+.\d+.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=application/octet-stream'},
-        {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=image/svg+xml'},
-        {test: /\.(jpe?g|png|gif)$/i, loader: 'file-loader?name=[name].[ext]'}
-    ]
+            },
+            { // compiles Sass to CSS
+              loader: 'sass-loader',
+              options: { sourceMap: true },
+            },
+          ],
+          // creates style nodes from JS strings
+          fallback: 'style-loader',
+        }),
+      }, // Should work for Bootstrap
+      { test: /\.eot(\?v=\d+.\d+.\d+)?$/, loader: 'file-loader' },
+      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff' },
+      { test: /\.[ot]tf(\?v=\d+.\d+.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=application/octet-stream' },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=image/svg+xml' },
+      { test: /\.(jpe?g|png|gif)$/i, loader: 'file-loader?name=[name].[ext]' },
+    ],
   },
   devServer: {
     historyApiFallback: true,
     // Messages like webpack bundle info is not shown: https://webpack.js.org/configuration/dev-server/#devserver-noinfo-
-    noInfo: true
+    noInfo: true,
   },
   plugins: [
-    // Create HTML file that includes references to bundled CSS and JS. 
+    // Create HTML file that includes references to bundled CSS and JS.
     new HtmlWebpackPlugin({
       template: 'src/index.html',
       minify: {
         removeComments: true,
-        collapseWhitespace: true
-      }
+        collapseWhitespace: true,
+      },
     }),
     // In production, create a separate CSS bundle file and disable this if dev
     new ExtractTextPlugin({
       filename: '[name].bundle.css',
-      disable: process.env.NODE_ENV === "development"
-    })
-  ]
+      disable: process.env.NODE_ENV === 'development',
+    }),
+  ],
 };
 
 /*
@@ -86,10 +87,10 @@ if (process.env.NODE_ENV === 'production') {
   config.plugins.push(
     new webpack.DefinePlugin({
       'process.env': {
-        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-      }
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+      },
     }),
-    new webpack.optimize.UglifyJsPlugin({ sourceMap: true })
+    new webpack.optimize.UglifyJsPlugin({ sourceMap: true }),
   );
 }
 
